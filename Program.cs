dbfd1246f920f6f24c7267c5644078d149827ed1
STONE17th <17th@inbox.ru>
2022-06-29 00:34:25 +0300

    Заполнение нового массива элементами меньше трех символов из исходного массива

--- Program.cs
+++ Program.cs
@@ -5,9 +5,17 @@ string DefaultData = Console.ReadLine() ?? "0";
 string [] DefaultArray = DefaultData.Split(",");
 
 int counter = 0;
+int i = 0;
 
 foreach (string Element in DefaultArray)
     if (Element.Length <= 3)
         counter++;
 
 string [] NewArray = new string [counter];
+
+foreach (string Element in DefaultArray)
+    if (Element.Length <= 3)
+    {
+        NewArray[i] = Element;
+        i++;
+    }
