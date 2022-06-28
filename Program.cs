61556b82f5e6711ed840f000ee1774c42a2cb3fc
STONE17th <17th@inbox.ru>
2022-06-29 00:33:08 +0300

    Создание нового массива размера равного колеству элементов меньше трех символов из исходного массива

--- Program.cs
+++ Program.cs
@@ -1,28 +1,13 @@
-﻿void PrintArray (string [] array)
-{
-    foreach (string Element in array)
-        Console.Write($"{Element}, ");
-    Console.WriteLine();
-}
-
-Console.Clear();
+﻿Console.Clear();
 Console.Write("Введите массив строк, разделяя элементы запятой: ");
 string DefaultData = Console.ReadLine() ?? "0";
 
 string [] DefaultArray = DefaultData.Split(",");
+
 int counter = 0;
-int i = 0;
 
 foreach (string Element in DefaultArray)
     if (Element.Length <= 3)
         counter++;
 
 string [] NewArray = new string [counter];
-foreach (string Element in DefaultArray)
-    if (Element.Length <= 3)
-    {
-        NewArray[i] = Element;
-        i++;
-    }
-PrintArray(DefaultArray);
-PrintArray(NewArray);
